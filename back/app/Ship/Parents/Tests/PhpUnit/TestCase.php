35e55f6b8ad2e9a60d089363f4b4cd56db556102
EugeneGpil <49585899+EugeneGpil@users.noreply.github.com>
2024-06-17 13:45:40 +0700

    OverrideSubdomain (#24)
    #23

--- back/app/Ship/Parents/Tests/PhpUnit/TestCase.php
+++ back/app/Ship/Parents/Tests/PhpUnit/TestCase.php
@@ -33,11 +33,6 @@ abstract class TestCase extends AbstractTestCase
      */
     public function createApplication()
     {
-        $this->baseUrl = env('API_FULL_URL'); // this reads the value from `phpunit.xml` during testing
-
-        // override the default subDomain of the base URL when subDomain property is declared inside a test
-        $this->overrideSubDomain();
-
         $app = require __DIR__ . '/../../../../../bootstrap/app.php';
 
         $app->make(ApiatoConsoleKernel::class)->bootstrap();
