04c367ac5fbfeab072cfd96e5112b7410cb5999d
EugeneGpil <gpilligrim@gmail.com>
2024-08-15 02:33:28 +0700

    set up front #42

---
+++ front/pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login-page">
+    <div class="login-box">
+      <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+          <a class="h1" href="#">
+            Agile<b>Forge</b>
+          </a>
+        </div>
+        <div class="card-body">
+          <p class="login-box-msg">Sign in to start your session</p>
+          <form>
+            <div class="input-group mb-3">
+              <input class="form-control" type="email" placeholder="Email">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-envelope" />
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
