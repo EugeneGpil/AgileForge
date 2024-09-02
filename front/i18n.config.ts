dd31d4321792eed1f87343412b238eaaff3a5eae
EugeneGpil <49585899+EugeneGpil@users.noreply.github.com>
2024-09-03 00:49:06 +0700

    make eslint to fix root files (#53)
    #52

--- front/i18n.config.ts
+++ front/i18n.config.ts
@@ -1,11 +1,12 @@
 import en from './i18n/en'
 import ru from './i18n/ru'
 
+// noinspection JSUnusedGlobalSymbols
 export default defineI18nConfig(() => ({
-    legacy: false,
-    locale: 'en',
-    messages: {
-        en,
-        ru,
-    },
+  legacy: false,
+  locale: 'en',
+  messages: {
+    en,
+    ru,
+  },
 }))
